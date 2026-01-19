461ce3fa698456006e24c59b5babf69d4ca0f8ac
Mirza Asim <mirzaasim2001@gmail.com>
2026-01-19 22:58:33 +0530

    UI delete conformation and Text change

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-ADMIN_USERNAME = "admin"
-ADMIN_PASSWORD = "iqra"
+ADMIN_USERNAME = "asimiqramirza"
+ADMIN_PASSWORD = "pfq9v8unq85qnpc!@#!@#9857qq87n5dq98"
 SECRET_KEY = "super_secret_key"
 AMAZON_AFFILIATE_TAG = "yourbrand-21"
